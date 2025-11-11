08036a10ac77b6613f63f49e739b77646f9e053e
suhaya11 <sulaimanhaladukadigawakibiya@gmail.com>
2025-11-11 23:51:11 +0100

    nwe commit

---
+++ sandbox.js
@@ -0,0 +1,13 @@
+var character = "suhaya";
+var circ = function (diameter) { return console.log(diameter * Math.PI); };
+circ(3);
+// arrays
+var names = ["sulaiman", "umar"];
+var mixid = [3, "duhsyu"];
+// names.push(3);
+// objects
+var person = {
+    name: "suahay",
+    age: 33,
+    ismarried: false,
+};
---
+++ sandbox.ts
@@ -0,0 +1,15 @@
+let character = "suhaya";
+let circ = (diameter: number) => console.log(diameter * Math.PI);
+circ(3);
+// arrays
+
+let names = ["sulaiman", "umar"];
+let mixid = [3, "duhsyu"];
+// names.push(3);
+// objects
+
+let person = {
+  name: "suahay",
+  age: 33,
+  ismarried: false,
+};
